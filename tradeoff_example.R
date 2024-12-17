1dcd57ddb10a695773e029f9d5e31e63e18d609a
Alyssa Chen <alyssachen@college.harvard.edu>
2024-12-17 11:44:59 -0500

    new mult X scenario

--- tradeoff_example.R
+++ tradeoff_example.R
@@ -341,9 +341,9 @@ make_data_multX <- function(n=100, trt_prop=.5, d_prop_trt=.1, d_prop_ctrl=.2, n
            d=1*I(id <= d_n_trt | (id > trt_n & id <= trt_n+d_n_ctrl) ), # subgroup indicator
            x1=rnorm(1, mean = 1 - 0.5*trt, sd = 1),
            x2=rnorm(1, mean = 1.2 - 0.6*trt + .2*d +.1*trt*d, sd = 1),
-           x3=rnorm(1, mean = 1.4 - 0.7*trt + .2*d +.1*trt*d, sd = 1),
-           x4=rnorm(1, mean = 1.6 - 0.8*trt + .2*d +.1*trt*d, sd = 1),
-           x5=rnorm(1, mean = 1.8 - 0.9*trt + .2*d +.1*trt*d, sd = 1),
+           x3=rnorm(1, mean = 1.2 - 0.6*trt + .2*d +.1*trt*d, sd = 1),
+           x4=rnorm(1, mean = 1.2 - 0.6*trt + .2*d +.1*trt*d, sd = 1),
+           x5=rnorm(1, mean = 1.2 - 0.6*trt + 1*d +1*trt*d, sd = 1),
            post=I(tp >= trt.time), # indicator of post-treatment period
            treated=I(post == 1 & trt == 1) # time-varying indicator if treated or not
     ) %>% 
@@ -351,7 +351,7 @@ make_data_multX <- function(n=100, trt_prop=.5, d_prop_trt=.1, d_prop_ctrl=.2, n
   
   dat <- dat %>% mutate(
     d_trt_effect = rnorm(1,-.2,sd=trt_effect_noise),
-    y0 = 1 + x1*tp + x2*tp + x3*(tp +.2*d)+  x4*(tp+.2*d+.1*d*tp) + x5*(tp+.2*d+.1*d*tp) + trt + int + ((tp - 2.5)^2)/10,
+    y0 = 1 + x1*tp + x2*tp + x3*(tp +.2*d+.1*d*tp)+  3*x4*(tp+.2*d+.1*d*tp) + x5*(tp+.2*d+.1*d*tp) + trt + int + ((tp - 2.5)^2)/10,
     y1 = y0 + (1+d_trt_effect*d),
     y = treated*y1+(1-treated)*y0  ) %>%
     group_by(id) %>% mutate(y.diff = y - lag(y)) %>% ungroup()
@@ -390,13 +390,17 @@ simanalyze_multX <- function(params){
     group_by(replicate) %>%
     nest() %>% 
     mutate(true_satt=map_dbl(data, true_satt)) %>% 
-    crossing(model_type = c("smallest","smaller","correct", "fuller", "fullest")) %>%
-    mutate(model_type = factor(model_type, levels=c("smallest","smaller","correct", "fuller", "fullest"))) %>% 
+    crossing(model_type = c("smallest","smaller_x3","smaller_x4","smaller_x5", "correct", "fuller1","fuller2","fuller3", "fullest")) %>%
+    mutate(model_type = factor(model_type, levels=c("smallest","smaller_x3","smaller_x4","smaller_x5", "correct", "fuller1","fuller2","fuller3", "fullest"))) %>% 
     mutate(model = 
-             case_when(model_type == "smallest" ~ map(data, ~lm(y ~ trt*post*d+x1+x2+x3+x4+x5, data = .) ),
-                       model_type == "smaller" ~ map(data, ~lm(y ~ trt*post*d+x1*post+x2+x3+x4*post+x5*post*d, data = .) ),
-                       model_type == "correct" ~ map(data, ~lm(y ~ trt*post*d+x1*post+x2*post+x3*post+x4*post*d+x5*post*d, data = .) ),
-                       model_type == "fuller" ~ map(data, ~lm(y ~ trt*post*d+x1*post+x2*post+x3*post*d+x4*post*d+x5*post*d, data = .) ),
+             case_when(
+                       model_type == "smallest" ~ map(data, ~lm(y ~ trt*post*d+x1*post+x2*post+x3*post+x4*post+x5*post, data = .) ),
+                       model_type == "smaller_x3" ~ map(data, ~lm(y ~ trt*post*d+x1*post+x2*post+x3*post*d+x4*post+x5*post, data = .) ),
+                       model_type == "smaller_x4" ~ map(data, ~lm(y ~ trt*post*d+x1*post+x2*post+x3*post+x4*post*d+x5*post, data = .) ),
+                       model_type == "smaller_x5" ~ map(data, ~lm(y ~ trt*post*d+x1*post+x2*post+x3*post+x4*post+x5*post*d, data = .) ),
+                       model_type == "correct" ~ map(data, ~lm(y ~ trt*post*d+x1*post+x2*post+x3*post*d+x4*post*d+x5*post*d, data = .) ),
+                       model_type == "fuller1" ~ map(data, ~lm(y ~ trt*post*d+x1*post*d+x2*post+x3*post*d+x4*post*d+x5*post*d, data = .) ),
+                       model_type == "fuller2" ~ map(data, ~lm(y ~ trt*post*d+x1*post+x2*post*d+x3*post*d+x4*post*d+x5*post*d, data = .) ),
                        model_type == "fullest" ~ map(data, ~lm(y ~ trt*post*d+x1*post*d+x2*post*d+x3*post*d+x4*post*d+x5*post*d, data = .) )
              )) %>% 
     mutate(coefs = map(model, broom::tidy)) %>%
@@ -418,10 +422,30 @@ ggplot(ests, aes(x=model_type, y=std.error)) +
   labs(title = "Standard error of misspecified and correct models",
        x="Model Type", y="Standard Error") 
 
+ggsave("plots/mult_varyX_se.png",width=6,height=4)
+
+
 ggplot(ests, aes(x=model_type, y=bias)) + 
   geom_boxplot() +
   labs(title = "Bias of misspecified and correct models",
        x="Model Type", y="Bias") 
+
+ggsave("plots/mult_varyX_bias.png",width=6,height=4)
+
+means <- ests %>% 
+  group_by(model_type) %>%
+  summarize(mean_bias = round(mean(bias),5), mean_se = round(mean(std.error),5))
+print(xtable(means),include.rownames=FALSE)
+
+means
+
+pre_data <- data %>% 
+  filter(post==FALSE) 
+lm(y~x3,pre_data)
+lm(y~x4,pre_data)
+lm(y~x5,pre_data)
+
+
 
 
 
@@ -484,6 +508,7 @@ for(i in 1:length(dtx_coefs)){
 
 plot(dtx_coefs, stderrs, xlab = "True dtx coefficient", ylab = "Standard Error of trt:post:d estimate")
 plot(dtx_coefs, ests, xlab = "True dtx coefficient", ylab = "trt:post:d estimate")
+
 
 
 
